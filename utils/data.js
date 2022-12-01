163f6e68d5077056cecec1cc612a74bbd0ca16e9
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-11-30 20:21:20 -0500

    added seed data

---
+++ utils/data.js
@@ -0,0 +1,40 @@
+const usernames = [
+    'Luffy',
+    'Zorro',
+    'Sanji',
+    'Ussop',
+    'Nami',
+    'Robin',
+    'Franky',
+    'Brook',
+    'Chopper',
+    'Jimbe',
+]
+
+const emails = [
+    'pirateking@email.com',
+    'oneeyethreeswords@email.com',
+    'cookingislife@email.com',
+    'sogeking@email.com',
+    'moneyispower@email.com',
+    'booksandflowers@email.com',
+    'roboman468@email.com',
+    'fortyfivedegrees@email.com',
+    'racoondog@email.com',
+    'seaknight@email.com',
+]
+
+const thoughts = [
+    "I want to be king of the pirates!!",
+    "I want to be the worlds greatest swordsman!!",
+    "I want to find the All Blue!!!",
+    "I want to be a true warrior of the sea!!",
+    "I want to draw a map of the whole world!!",
+    "I want to learn all of the history of the world!!",
+    "I want my ship to travel the whole world!!",
+    "I want to meet LABOOOOOOOOOOON!!!!",
+    "I want to become a doctor that can cure any disease!!",
+    "I want fishmen and humans to live in harmony and peace!!",
+]
+
+module.exports = { usernames, emails, thoughts };
